9787d09b6b1170b373eef8ee8b6c0c9d5ca4d4a7
jagruti892 <gadekarjagruti443@gmail.com>
2026-08-20 02:23:33 -0400

    Update Vehicle.py

--- python/Vehicle.py
+++ python/Vehicle.py
@@ -6,8 +6,8 @@ from abc import ABC, abstractmethod
 class Vehicle(ABC):
 #ABC → allows us to create an Abstract Base Class.
 #abstractmethod → marks a method as abstract.
-    def __init__(self, vehicleId, vehicleNumber, capacity):
-        self.vehicleId = vehicleId
+    def __init__(self, vehicleId, vehicleNumber, capacity):  #constructor
+        self.vehicleId = vehicleId   #self means: the current object
         self.vehicleNumber = vehicleNumber
         self.capacity = capacity
 
