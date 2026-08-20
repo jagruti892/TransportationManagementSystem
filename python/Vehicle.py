6f4c68c826b5d4cc3834d8a8bb520b92caabf3b0
jagruti892 <gadekarjagruti443@gmail.com>
2026-08-20 02:02:29 -0400

    Fix comments formatting in Vehicle.py

--- python/Vehicle.py
+++ python/Vehicle.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 
 class Vehicle(ABC):
-//allows us to create an Abstract Base Class.
+# allows us to create an Abstract Base Class.
     def __init__(self, vehicleId, vehicleNumber, capacity):
         self.vehicleId = vehicleId
         self.vehicleNumber = vehicleNumber
